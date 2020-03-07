c8b115f2d9f7fd8c337c6c76eae095dddfeb6080
bimonestle <bimonestle@yahoo.co.id>
2020-03-07 18:08:25 +0700

    Create normalize(phone string)

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"bytes"
+)
+
+//
 func normalize(phone string) string {
-	return phone
+	// The Correct format - 0123456789
+	// It contains numbers only
+
+	var buf bytes.Buffer
+	for _, ch := range phone {
+		// If the string contains between these runes
+		if ch >= '0' && ch <= '9' {
+			buf.WriteRune(ch) // write it into the Buffer
+		}
+	}
+	return buf.String() // convert it back into string
 }
